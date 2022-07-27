3ec56d107bd0444a492224cb0f03a5ac678369b2
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-27 15:09:29 +0800

    created login route

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", userController.myRequestListener);
 app.post("/signup", userController.addNewUser);
+app.post("/login", userController.checkUser);
 
 app.listen(3004);
