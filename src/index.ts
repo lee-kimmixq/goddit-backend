83480adbe379734fc2ffa2f1f7cee2a2996e8f40
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-27 15:54:59 +0800

    added dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import passport from "passport";
+import dotenv from "dotenv";
 import userController from "./controller/userController";
+
+dotenv.config();
 
 mongoose.connect("mongodb://localhost/goddit");
 
 const app: Express = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
 app.get("/", userController.myRequestListener);
 app.post("/signup", userController.addNewUser);
