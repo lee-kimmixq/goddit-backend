a10f64e54955a916fc9c998b132554b83044866f
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-27 13:56:22 +0800

    added post route for signup

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,6 @@ const app: Express = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", userController.myRequestListener);
+app.post("/signup", userController.addNewUser);
 
 app.listen(3004);
